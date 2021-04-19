40e13c3a1b0fbcb306277b19197ad001fce8d7fb
Gopal Bhaire <GopalBhaire@gmail.com>
2021-04-19 22:12:34 +0530

    Add: logic to get room and name

--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Join(props) {
-  return <h1>Join</h1>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleRoomChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <div className="main-container">
+      <div className="container">
+        <div>
+          <input
+            placeholder="Name"
+            className="name"
+            type="text"
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="room"
+            type="text"
+            onChange={handleRoomChange}
+          />
+        </div>
+        <Link>
+          <button className="button" />
+        </Link>
+      </div>
+    </div>
+  );
 }
