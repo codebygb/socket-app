c988e21518b496ba9d1bd6e4ff2a547931935717
Gopal Bhaire <GopalBhaire@gmail.com>
2021-04-21 23:49:17 +0530

    Add putout

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
-const socketio = require("socket.io");
-const io = socketio(server, {
+const io = require("socket.io")(server, {
   cors: {
     origin: `http://localhost:3000`,
     methods: ["GET", "PUT", "POST"],
   },
 });
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
@@ -26,7 +26,11 @@ io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, cb) => {
     const { user, error } = addUser(socket.id, user, name);
     if (error) return cb(error);
-    socket;
+    socket.emit("message", {
+      user: "admin",
+      text: `Welcome to ${room}, ${user}`,
+    });
+    socket.join(user.room);
   });
   socket.on("disconnect", () => {
     console.log("Disconnected");
