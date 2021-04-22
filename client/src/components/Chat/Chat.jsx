a19f4beff8b3856a3cbcad172f21f239937af4c4
Gopal Bhaire <GopalBhaire@gmail.com>
2021-04-22 21:08:30 +0530

    Add: on message socket event

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect } from "react";
-import { io } from "socket.io-client";
 import PropTypes from "prop-types";
-
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 import "./Chat.css";
 
 let socket;
 const ENDPOINT = "localhost:5000";
 
 function Chat(props) {
-  const {
-    name,
-    room
-  } = props;
+  const { name, room } = props;
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     socket = io(ENDPOINT);
@@ -21,6 +19,12 @@ function Chat(props) {
       socket.off();
     };
   }, [ENDPOINT, name]);
+
+  useEffect(() => {
+    socket.on("message", (message) =>
+      setMessages((prev) => [...prev, message])
+    );
+  }, [messages]);
 
   return <h1>Chat</h1>;
 }
